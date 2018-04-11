615be53190f033671a096327a53dfa8c243238f3
Manuel Grullon <astermanuelg@gmail.com>
2018-04-11 18:00:27 -0400

    Great Group

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,32 @@
 class DogsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :age, :breed)
+  end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,51 @@
 class EmployeesController < ApplicationController
+  def index
+  end
+
+  def new
+    @dogs = Dog.all
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    @employee.save
+    if @employee.valid?
+      redirect_to @employee
+    else
+      redirect_to new_employee_path(@employee)
+    end
+
+  end
+
+  def edit
+    @dogs = Dog.all
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+
+    @employee = Employee.find(params[:id])
+    @employee.update(employee_params)
+    if @employee.valid?
+      redirect_to @employee
+    else
+      render :new
+    end
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def destroy
+  end
+
+
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id )
+  end
 end
